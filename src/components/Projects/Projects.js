228d252ad08b5a0baadc5eff41b7b1feee508f8a
vivek kumar <vkpvivek1998@gmail.com>
2023-07-24 15:27:48 +0530

    done project
    done project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,7 +21,7 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          { <Col md={4} className="project-card">
+          {  /*<Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
@@ -32,7 +32,7 @@ function Projects() {
             />
           </Col>
 
-        /*  <Col md={4} className="project-card">
+         <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
