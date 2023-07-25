9d7a9a17f91d6232b271e7b88b66dd135cc5b0d3
vivek kumar <vkpvivek1998@gmail.com>
2023-07-25 11:15:46 +0530

    updated about
    updated about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+            Unveil my true  <strong className="purple">self.</strong>
             </h1>
             <Aboutcard />
           </Col>
