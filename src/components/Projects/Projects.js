07268c453630f36e1059ac14c384a99feb8e5c76
vivek kumar <vkpvivek1998@gmail.com>
2023-08-24 11:56:39 +0530

    project added
    project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import movie from "../../Assets/Projects/movie.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -21,17 +21,17 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          {  /*<Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={movie}
               isBlog={false}
-              title="Chatify"
+              title="movieDB"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/kumarvivekpandey/movieapi"
+              demoLink="https://movieapi.kumarvivekpande.repl.co/home"
             />
           </Col>
-
+          {  /*
          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
