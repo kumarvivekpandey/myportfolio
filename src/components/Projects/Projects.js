51b0fd2fb9633fcbd1109a5c9ac8b62cebf945f1
Vivek Kumar <35356580+kumarvivekpandey@users.noreply.github.com>
2023-09-14 12:36:51 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import apna from "../../Assets/Projects/apna.png";
 import movie from "../../Assets/Projects/movie.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
@@ -48,12 +48,12 @@ function Projects() {
              {  /*
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={apna}
               isBlog={false}
               title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              description="Online MERN E-Commerce - Admin Panel, Redux Toolkit, Payment Gateway, Cloud Deployment, Emails"
+              ghLink="https://github.com/kumarvivekpandey/Apnabazaar"
+              demoLink="https://apnabazaar-mu.vercel.app/"              
             />
           </Col>
 
